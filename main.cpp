ceae3b02ab46c446ca2b6937f7b9a9e4a9eb855d
Stefan Hagen <stefan@hagen.link>
2021-09-15 09:43:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,42 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
+#include <array>
+#include <string>
+#include <sstream>
+
+#include <doctest/doctest.h>
+
+#include <nlohmann/json.hpp>
+
+
+TEST_SUITE ("Example derived tests.") {
+    TEST_CASE ("No error in parsing the JSON fixture with round trip.") {
+        /* Example snippet:
+         *
+         * auto j = R"(
+         *   {
+         *     "happy": true,
+         *     "pi": 3.14156
+         *   }
+         * )"_json;
+         * std::string s = j.dump();
+         * std::cout << "compact:  " << s << "\n";
+         * std::cout << "indented:\n" 
+         *           << j.dump(4) << "\n";
+         */
+        auto j = R"(
+          {
+            "happy": true,
+            "pi": 3.14156
+          }
+        )"_json;
+        SUBCASE("The JSON representation is not empty in round trip.") {
+            std::string s = j.dump();
+            REQUIRE(!s.empty());
+        }
+        SUBCASE("The indentation on dump works in round trip.") {
+            std::string si = j.dump(42);
+            REQUIRE(si.find("                                          ") != std::string::npos);
+        }
+    }
+}
