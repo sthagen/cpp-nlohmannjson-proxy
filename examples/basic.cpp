dcf5add63ff016739b059056080c5ad6ef073f87
Stefan Hagen <stefan@hagen.link>
2021-09-15 09:38:13 +0200

    Update basic.cpp

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include <string>
+
+#include <nlohmann/json.hpp>
+
+
+int main() {
+  auto j = R"(
+    {
+      "happy": true,
+      "pi": 3.14156
+    }
+  )"_json;
+  std::string s = j.dump();
+
+  std::cout << "compact:  " << s << "\n";
+  std::cout << "indented:\n" 
+            << j.dump(4) << "\n";
+  
+}
